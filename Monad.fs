c56ad9607fc609d43d332436d736c54999c3fa3e
GuillaumeLaf <guillaume1.lafontaine@gmail.com>
2021-08-20 16:49:10 +0200

    Relocate Monad module to extand use beyong backtester

--- Monad.fs
+++ Monad.fs
@@ -26,3 +26,23 @@
             let x, nextState2 = run mx nextState
             (f x), nextState2
         M innerFunc
+
+    let rec foldr folder list initState k = 
+        match list with
+        | [] -> k initState
+        | x::xs -> foldr folder xs initState (fun r -> k (folder x r)) 
+
+    let traverse f list =
+        let (>>=) x f = bind f x
+        let cons hd tl = hd :: tl
+
+        let initState = rets []
+        let folder head tail =
+            f head >>= (fun h ->
+            tail >>= (fun t ->
+            rets (cons h t) ))
+
+        List.foldBack folder list initState
+        //foldr folder list initState id
+
+    let sequence list = traverse id list
