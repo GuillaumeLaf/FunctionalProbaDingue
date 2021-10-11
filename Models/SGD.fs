3b1de6923e5b4bddec9bf113f275646f012c4cee
GuillaumeLaf <63575792+GuillaumeLaf@users.noreply.github.com>
2021-10-11 21:24:44 +0200

    SGD with momentum not great implementation

--- Models/SGD.fs
+++ Models/SGD.fs
@@ -8,19 +8,18 @@ module SGD =
     let (<!>) = Monad.map
     let (>>=) x f = Monad.bind f x
 
+    type Optimizer = 
+        | Classic of learningRate:float
+        | Momentum of momentumRate:float * momentumValue:float [] * learningRate:float
+        | RMSProp of momentumRate:float * momentumValue:float [] * learningRate:float
+        
     let limitParams x =
         match x with
         | a when x < -1.0 -> -1.0
         | a when x > 1.0 -> 1.0
         | _ -> x
-    
-(*    let updateRuleM learningRate skeleton parameterIdx parameterValue currentError = 
-        Monad.state {
-            return parameterValue - learningRate * gradient * currentError
-        }
-*)
 
-    let gradientUpdateM skeleton idx = 
+    let currentErrorGradientM skeleton idx = 
         Monad.state {
             do! GraphTS.runTimeSeriesM (TimeSeries.Univariate.setCurrentIndexM idx)
             let! currentError = GraphTS.runTimeSeriesM (TimeSeries.Univariate.currentInnovationM ())
@@ -29,24 +28,34 @@ module SGD =
             return Array.map (fun g -> g * currentError) currentGradient
         }
 
-    let multipleGradientUpdateM nParameters skeleton indices = 
+    let aggregateCurrentErrorGradientM nParameters skeleton indices = 
         Monad.state {
             let gradient = Array.zeroCreate nParameters
-            let! currentGradient = Array.map (fun idx -> gradientUpdateM skeleton idx) indices |> Monad.mapM
-            return Array.foldBack (fun x s -> UtilitiesSIMD.ArraySIMD.add x s) currentGradient gradient
+            let! currentGradients = Array.map (fun idx -> currentErrorGradientM skeleton idx) indices |> Monad.mapM
+            let aggregateGradient = Array.foldBack (fun x s -> UtilitiesSIMD.ArraySIMD.add x s) currentGradients gradient
+            return UtilitiesSIMD.ArraySIMD.multScalar (1.0/float nParameters) aggregateGradient
         }
 
+    let updatedOptimizerAndParameters parameterValues gradient = function
+        | Classic(learningRate) as x -> x, Array.map2 (fun p g -> p - learningRate * g) parameterValues gradient
+        | Momentum(momRate,momValue,learnRate) -> let newMomentum = Array.map2 (fun mm g-> momRate*mm - learnRate*g) momValue gradient
+                                                  Momentum(momRate,newMomentum,learnRate), UtilitiesSIMD.ArraySIMD.add parameterValues newMomentum
+        | RMSProp(momRate,momValue,learnRate) -> let newMomentum = Array.map2 (fun mm g -> momRate*mm + (1.0-momRate)*g*g) momValue gradient
+                                                 RMSProp(momRate,newMomentum,learnRate), Array.map3 (fun p mm g -> p - g*(learnRate/(sqrt(mm)+0.0001)))
+                                                                                                    parameterValues momValue gradient
+
     // Updating of the parameters must be made after running the model.
-    let updateParametersM learningRate skeleton indices = 
+    let updateParametersM optimizer skeleton indices = 
         Monad.state {
             let! parameterValues = GraphTS.runGraphM Graph.parametersM
-            let! gradient = multipleGradientUpdateM (parameterValues.Length) skeleton indices
-            let newParameters = Array.map2 (fun p g -> p - learningRate * g) parameterValues gradient
+            let! gradient = aggregateCurrentErrorGradientM (parameterValues.Length) skeleton indices
+            let newOptimizer,newParameters = updatedOptimizerAndParameters parameterValues gradient optimizer
             let newParameters = newParameters |> Array.map (fun x -> limitParams x)
             do! GraphTS.runGraphM (Graph.setParametersM newParameters)
+            return newOptimizer
         }
             
-    let fit model learningRate epochs array = 
+    let fit model optimizer epochs array = 
         let model = (ErrorModel(model))
         let initStateTS = TimeSeries.Univariate.defaultStateFrom array
         let initStateG = Graph.defaultStateForFitting model
@@ -54,14 +63,16 @@ module SGD =
         let errorSkM = model |> Fitting |> Graph.modelM
         let updateVarM = GraphTS.updateForFittingM model
 
-        let fittingM indices = 
+        let fittingM opt indices = 
             Monad.state {
-                do! GraphTS.SDGfitM updateVarM errorSkM indices
-                do! updateParametersM learningRate defaultSk indices
+                do! GraphTS.SDGfitM updateVarM errorSkM indices // compute the error
+                return! updateParametersM opt defaultSk indices
             }
-                                  
-        let folder states = Array.fold (fun s idx -> let _,nxtStates = s |> Monad.run (fittingM idx) 
-                                                     nxtStates) states                              
+                         
+        // function that will fold on the indices. 
+        let folder opt states = Array.fold (fun s idx -> let newOpt,newStates = s
+                                                         let nxtOpt,nxtStates = newStates |> Monad.run (fittingM newOpt idx) 
+                                                         (nxtOpt,nxtStates)) (opt,states)                              
         
         let printCurrent states = 
             let (Graph.State(p,_,_),TimeSeries.Univariate.State(_,_,errors)) = states
@@ -70,6 +81,6 @@ module SGD =
 
         let rec loop idxArray epochs states = 
             match epochs with
-            | 1 -> printfn "%A" (printCurrent states); folder states (idxArray |> Array.chunkBySize 12)
-            | _ -> printfn "%A" (printCurrent states); loop (Utilities.shuffle idxArray) (epochs-1) (folder states (idxArray |> Array.chunkBySize 12))
-        loop (Array.init array.Length id) epochs (initStateG,initStateTS) |> fst
+            | 1 -> printfn "%A" (printCurrent states); folder optimizer states (idxArray |> Array.chunkBySize 1)
+            | _ -> printfn "%A" (printCurrent states); loop (Utilities.shuffle idxArray) (epochs-1) (folder optimizer states (idxArray |> Array.chunkBySize 1) |> snd)
+        loop (Array.init array.Length id) epochs (initStateG,initStateTS) |> snd |> fst
--- Program.fs
+++ Program.fs
@@ -10,13 +10,13 @@ open FSharp.Charting
 let main argv =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     
-    let model = STARp([|0.6;-0.2|],[|-0.9;0.4|],0.0,1.0,ARp([|0.7|])) |> Sampling
-    //let model = ARp([|0.7|]) |> Sampling
+    //let model = STARp([|0.6;-0.2|],[|-0.9;0.4|],0.0,1.0,ARp([|0.7|])) |> Sampling
+    let model = ARp([|0.7|]) |> Sampling
     let sple = GraphTS.sample 1000 model
     let sple = Array.map (fun x -> Some x) sple
 
-    //printfn "%A" (SGD.fit (AR(1)) 0.0001 100 sple)
-    printfn "%A" (SGD.fit (STAR(2,0.0,1.0,AR(1))) 0.005 100 sple)
+    printfn "%A" (SGD.fit (AR(1)) (SGD.RMSProp(0.9,[|0.0|],0.0001)) 500 sple)
+    //printfn "%A" (SGD.fit (STAR(2,0.0,1.0,AR(1))) (SGD.Classic(0.003)) 100 sple)
 
 
     
