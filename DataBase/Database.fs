9d8eed5ea599161bf6560451ce71fd393d8aa0d0
GuillaumeLaf <63575792+GuillaumeLaf@users.noreply.github.com>
2022-02-25 08:06:40 +0100

    Database Refactoring and Queries

---
+++ DataBase/Database.fs
@@ -0,0 +1,107 @@
+﻿namespace Database
+
+open System
+open System.Data
+open FSharp.Data.Sql
+open System.IO
+open System.Linq
+
+module DB = 
+    let path = "C:\Users\Guillaume\OneDrive\Trading\FSharp\Data\Binance"
+
+    type sqlReal = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, "Server=localhost;Database=BinanceDB;User Id=sa;Password=123">
+
+    let ctxReal = sqlReal.GetDataContext()
+
+    [<RequireQualifiedAccess>]
+    module Table = 
+        
+        [<RequireQualifiedAccess>]
+        module Timeseries = 
+            
+            [<Struct>]
+            type Key = Key of string * DateTime
+
+            type Data = 
+                { Ticker:string;
+                  CloseTime:DateTime;
+                  OpenPrice:float;
+                  HighPrice:float;
+                  LowPrice:float;
+                  ClosePrice:float;
+                  QuoteVolume:float;
+                  BaseVolume:float;
+                  TradeCount:int;
+                  OpenTime:DateTime }
+
+            let context = ctxReal.Dbo.TTimeSeries
+
+            let createRow (data:Data) = 
+                let t = context.Create()
+                t.Ticker <- data.Ticker
+                t.CloseTime <- data.CloseTime
+                t.OpenPrice <- data.OpenPrice
+                t.HighPrice <- data.HighPrice
+                t.LowPrice <- data.LowPrice
+                t.ClosePrice <- data.ClosePrice
+                t.QuoteVolume <- data.QuoteVolume
+                t.BaseVolume <- data.BaseVolume
+                t.TradeCount <- data.TradeCount
+                t.OpenTime <- data.OpenTime
+                ctxReal.SubmitUpdates()
+
+            [<RequireQualifiedAccess>]
+            module Query = 
+
+                let get ticker from_ to_ = 
+                    query {
+                        for p in context do 
+                        where (p.Ticker=ticker && p.OpenTime>=from_ && p.OpenTime<to_)
+                        select p
+                    } 
+
+                let closePrice ticker from_ to_ = 
+                    query {
+                        for p in context do
+                        where (p.Ticker=ticker && p.OpenTime>=from_ && p.OpenTime<to_)
+                        select (p.CloseTime, [|p.ClosePrice|])
+                    }
+                    
+                // Time is given by the first ticker ! should change to get the maximum time available for the given tickers. 
+                let closePrices (tickers:string[]) from_ to_ =
+                    let mutable tmp = closePrice tickers.[0] from_ to_
+                    for t in Array.tail tickers do 
+                        tmp <- query {
+                                for (t1,arr) in tmp do
+                                join (t2,p2) in closePrice t from_ to_ on (t1=t2)
+                                select (t1,Array.append arr p2)
+                               }
+                    tmp
+                        
+                
+
+            let closePrices tickers from_ to_ = Query.closePrices tickers from_ to_ |> Seq.toArray
+
+            let multipleGet ticker1 ticker2 from_ to_ = 
+                query {
+                    for (t1,p1) in Query.closePrice ticker1 from_ to_ do
+                    join (t2,p2) in Query.closePrice ticker2 from_ to_ on (t1 = t2)
+                    select (t1,[|p1;p2|])
+                } |> Seq.toArray
+
+            let get ticker from_ to_ = Query.get ticker from_ to_ |>Seq.map (fun x -> x.MapTo<Data>())
+                
+            let getClosePrice ticker from_ to_ = Query.closePrice ticker from_ to_ |> Seq.toArray
+
+        [<RequireQualifiedAccess>]
+        module Tickers = 
+
+            type Key = Key of string
+            
+            let tTickersCtx = ctxReal.Dbo.TTickers
+
+
+
+
+
+
--- Main.fs
+++ Main.fs
@@ -6,17 +6,21 @@ open ComputationalGraph.GraphType
 open Models
 open Models.ModelType
 open Utils
+open Database
 
 [<EntryPoint>]
 let main argv =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 
     // let g = Input(Parameter(0,0)) + Input(Parameter(0,1)) * (Input(Parameter(0,1))*Input(Parameter(0,2)) + Input(Parameter(1,2)))
-    let dgp = (VAR({n=2; order=1; parameters=Some([|Array2D.ofArray [|[|0.7f;-0.2f|];[|0.1f;-0.5f|]|]|]); covariance=Some(Array2D.ofArray [|[|1.0f;0.0f|];[|0.0f;1.0f|]|])}))
+(*    let dgp = (VAR({n=2; order=1; parameters=Some([|Array2D.ofArray [|[|0.7f;-0.2f|];[|0.1f;-0.5f|]|]|]); covariance=Some(Array2D.ofArray [|[|1.0f;0.0f|];[|0.0f;1.0f|]|])}))
     let m = Model.create dgp
     let sampleModel = Model.sample 1000 m
     let fitted = Model.fit sampleModel (Optimisation.Optimizer.Momentum(0.005f, 0.9f)) 100
-    printfn "%A" fitted
+    printfn "%A" fitted*)
+
+    Database.DB.Table.Timeseries.closePrices [|"1INCHBTC";"1INCHBUSD";"1INCHDOWNUSDT"|] (new DateTime(2021,9,1)) (new DateTime(2021,9,5)) |> printfn "%A"
+    
     
     stopWatch.Stop()
     printfn "%f seconds elapsed" (stopWatch.Elapsed.TotalMilliseconds / 1000.0)
