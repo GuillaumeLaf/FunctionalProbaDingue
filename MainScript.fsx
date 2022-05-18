6ef788683afd9589428019c7798bcc8feb037092
GuillaumeLaf <63575792+GuillaumeLaf@users.noreply.github.com>
2022-05-18 07:29:38 +0200

    Cross validation splits - fixed and rolling.

--- MainScript.fsx
+++ MainScript.fsx
@@ -1,15 +1,27 @@
 ﻿#r "nuget: FSharpPlus, 1.2.2"
+#r "nuget: MathNet.Numerics"
+#r "nuget: MathNet.Numerics.FSharp"
+#r "nuget: FSharp.Charting"
 #load "Utils.fs"
 #load "ComputationalGraph\GraphType.fs"
 #load "ComputationalGraph\BasicInput.fs"
-#load "ComputationalGraph\StateGraph.fs"
+#load "ComputationalGraph\GraphState.fs"
 #load "ComputationalGraph\Graph.fs"
 #load "ComputationalGraph\Node.fs"
 
-#load "TimeSeries\Statistics.fs"
 #load "TimeSeries\TimeseriesType.fs"
-#load "TimeSeries\TimeSeries.fs"
 #load "TimeSeries\TimeseriesState.fs"
+#load "TimeSeries\Statistics.fs"
+#load "TimeSeries\Transformations.fs"
+
+#load "Model\ModelType.fs"
+#load "Model\ModelState.fs"
+#load "Model\Optimisation.fs"
+#load "Model\Model.fs"
+#load "Model\Model_Selection\CrossValidation.fs"
+
+
+
 
 
 
--- Model/Model_Selection/CrossValidation.fs
+++ Model/Model_Selection/CrossValidation.fs
@@ -12,21 +12,24 @@ module Timeseries =
     // 'Leave-P-Out' bounds computation
     // Output an 'Seq' of tuples : ((lowerBoundTrain,upperBoundTrain),(lowerBoundTest,upperBoundTest))
     // The sequence is walking forward by one each time.
-    let rollingBoundsLPO (minLength:int) (p:int) = Seq.initInfinite (fun i -> (0,i+minLength-1),(i+minLength,i+minLength+p))
-    let fixedBoundsLPO (minLength:int) (p:int) = Seq.initInfinite (fun i -> (i,i+minLength-1),(i+minLength,i+minLength+p))
+    let rollingBoundsLPO (minLength:int) (overlap:int) (p:int) = Seq.initInfinite (fun i -> (0,i+minLength-1),(i+minLength-overlap,i+minLength+p-1))
+    let fixedBoundsLPO (minLength:int) (overlap:int) (p:int) = Seq.initInfinite (fun i -> (i,i+minLength-1),(i+minLength-overlap,i+minLength+p-1))
 
     // 'Leave-One-Out'
-    let rollingBoundsLOO = flip rollingBoundsLPO 0
-    let fixedBoundsLOO = flip fixedBoundsLPO 0
+    let rollingBoundsLOO minLength overlap = rollingBoundsLPO minLength overlap 1
+    let fixedBoundsLOO minLength overlap = fixedBoundsLPO minLength overlap 1
        
-
+    let fixedSplit (foldLength:int) (overlap:int) = fixedBoundsLPO foldLength overlap foldLength
+                                                        |> Seq.choose (fun x -> if (fst >> fst) x % foldLength = 0 then Some x else None)
+        
+    let rollingSplit (foldLength:int) (overlap:int) = rollingBoundsLPO foldLength overlap foldLength
+                                                        |> Seq.choose (fun x -> if ((fst >> snd) x - foldLength+1) % foldLength = 0 then Some x else None)
 
     let crossValidationSplit = function
         | Fixed -> fixedSplit
         | Rolling -> rollingSplit
         
     
-
 module CrossValidation = 
     // https://scikit-learn.org/stable/modules/cross_validation.html
     let x = 0
